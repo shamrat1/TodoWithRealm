0226acbf840c93fe34a8cfbb73d8305fcfa9517c
Yasin Shamrat <yshamrat@gmail.com>
2020-01-01 10:30:57 +0600

    Initial Commit

---
+++ TodoWithRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoWithRealm
+//
+//  Created by Yasin Shamrat on 1/1/20.
+//  Copyright © 2020 Yasin Shamrat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
